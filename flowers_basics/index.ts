0eb0339ea4d4a98f5fb099d83bc2febb28d50ce7
Rainer Stropek <rainer@timecockpit.com>
2025-10-10 08:33:41 +0200

    Saved in TypeScript Playground at 2025-10-10T06:33:40.898Z

---
+++ flowers_basics/index.ts
@@ -0,0 +1,24 @@
+// <<< ADD CONSTANTS HERE (if you need them)
+
+function setup() {
+  createCanvas(1000,1000)
+  background("lightgrey")
+
+
+fill("lightgreen")
+circle(750, 600 80)
+
+fill("lightgreen")
+circle(700, 550, 80)
+
+fill("lightgreen")
+circle(750,500 )
+
+fill("lightgreen")
+circle()
+
+arc(750,750, 100, 100,0,0)
+
+
+  // <<< ADD YOUR CODE HERE
+}
