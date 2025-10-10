f31fcb7f0626d7b2b2fe63a4e8af966738a2bb04
Rainer Stropek <rainer@timecockpit.com>
2025-10-10 09:27:45 +0200

    Saved in TypeScript Playground at 2025-10-10T07:27:45.336Z

--- flowers_basics/index.ts
+++ flowers_basics/index.ts
@@ -4,13 +4,13 @@ function setup() {
   createCanvas(1000, 1000)
   background("lightgrey")
 
-angleMode(DEGREES)
+  angleMode(DEGREES)
   noFill()
   stroke("green")
   strokeWeight(20)
-  arc(450,725, 800,500, -21,15)
-  
-  
+  arc(450, 725, 800, 500, -21, 15)
+
+  strokeWeight(1)
   fill("lightgreen")
   circle(750, 600, 80)
 
@@ -26,6 +26,28 @@ angleMode(DEGREES)
   fill("yellow")
   noStroke()
   circle(750, 550, 65)
+
+
+  //zweite Blume
+  
+  fill("lightgreen")
+  circle(550, 550, 70)
+
+  fill("lightgreen")
+  circle(515.5, 597.5,70)
+
+  fill("lightgreen")
+  circle(456,581.9, 70)
+
+  fill("lightgreen")
+  circle(515.5,502.5,70)
+
+  fill("lightgreen")
+  circle(456, 518.1, 70)
+
+  fill("yellow")
+  circle(500,550, 65)
+  
 
 
 
