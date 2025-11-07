6a2228550870a64196362fb68b8fe7b0a008823f
Rainer Stropek <rainer@timecockpit.com>
2025-11-07 09:40:23 +0100

    Saved in TypeScript Playground at 2025-11-07T08:40:22.870Z

--- rect_tool_exam/index.ts
+++ rect_tool_exam/index.ts
@@ -4,17 +4,28 @@ function setup() {
     angleMode(DEGREES);
 }
 
-function mouseMoved(){
+function mouseMoved() {
     background("black")
     stroke('white')
     noFill()
-    
-    let w= (mouseX-width/2) *2
-    let h= (mouseY-height/2)*2
-   
-    rect(width /2-w, height / 2-h, w, h)
 
-    
-    const area= Math.round(w /2 * h/2 * Math.PI)
-    text(`Area: ${area}`, 5, height-10)
+    let w = (mouseX - width / 2) * 2
+    let h = (mouseY - height / 2) * 2
+
+    rect(width / 2 - w / 2, height / 2 - h / 2, w, h)
+
+    stroke("lightgrey")
+    strokeWeight(1)
+    line(width / 2 - w / 2, height / 2, width / 2 + w / 2, height / 2);
+    line(width / 2, height / 2 - h / 2, width / 2, height / 2 + h / 2);
+
+    noStroke();
+    fill("lightgray");
+    textAlign(RIGHT);
+    text(`${w}`, width / 2 - w / 2 - 5, height / 2 - 5);
+    text(`${h}`, width / 2, height / 2 - h / 2 - 5);
+
+    textAlign(LEFT)
+    const area =Math.round (w * h)
+    text(`Area: ${area}`, 5, height - 10)
 }
