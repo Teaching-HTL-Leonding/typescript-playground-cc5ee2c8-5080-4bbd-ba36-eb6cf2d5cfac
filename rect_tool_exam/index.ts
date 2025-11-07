f0d487b23092be885bc78284952a616328ba5f77
Rainer Stropek <rainer@timecockpit.com>
2025-11-07 08:44:43 +0100

    Saved in TypeScript Playground at 2025-11-07T07:44:42.758Z

--- rect_tool_exam/index.ts
+++ rect_tool_exam/index.ts
@@ -4,13 +4,18 @@ function setup() {
     angleMode(DEGREES);
 }
 
-funtcion mouseMoved(){
+function mouseMoved(){
+    const gridSize= 20
     background("black")
     stroke('white')
     noFill()
     
-    let w= (mouseX-width/2) *2
-    let h= (mouseY-height/2)*2
+    let w= Math.abs(mouseX-width/2) *2
+    w= Math.round(w/gridSize)
+    let h= Math.abs(mouseY-height/2)*2
     
     rect(width / 2, height / 2,w,h)
+
+    const area= Math.round(w /2 * h/2 * Math.PI)
+    text(`Area: ${area}`, 5, height-10)
 }
