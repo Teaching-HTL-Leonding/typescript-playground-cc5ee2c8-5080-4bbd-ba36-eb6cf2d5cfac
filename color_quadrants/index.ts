252646ebbe82d3072e365d19ab59f33d40725ead
Rainer Stropek <rainer@timecockpit.com>
2025-11-21 08:42:19 +0100

    Saved in TypeScript Playground at 2025-11-21T07:42:19.055Z

---
+++ color_quadrants/index.ts
@@ -0,0 +1,59 @@
+function setup() {
+  createCanvas(400, 200);
+  background("black");
+  noStroke();
+
+  // Vier Quadranten
+  fill("orange");
+  rect(0, 0, width/2, height/2);
+
+  fill("green");
+  rect(width/2, 0, width/2, height/2);
+
+  fill("blue");
+  rect(0, height/2, width/2, height/2);
+
+  fill("red");
+  rect(width/2, height/2, width/2, height/2);
+}
+
+// <<< Add your code here
+function moveClicked() {
+  // Schwarzer Balken in der Mitte
+  const textAreaHeight = 50;
+  const textAreaWidth  = 200;
+
+  // Bereich leeren
+  fill("black");
+  rect(width/2 - textAreaWidth/2, height/2 - textAreaHeight/2,
+       textAreaWidth, textAreaHeight);
+
+  noStroke();
+  fill("white")
+  textSize(30)
+  textStyle(BOLD)
+  textAlign(CENTER, CENTER)
+
+  let message: string
+
+  if (mouseX < width/2) {
+    // linke Hälfte
+    if (mouseY < height/2) {
+      message = "Orange"
+    } else {
+      message = "Blue"
+    }
+  } else {
+    if (mouseY < height/2) {
+      message = "Green"
+    } else {
+      message = "Red"
+    }
+  }
+
+  text(message, width/2, height/2);
+}
+
+function mouseClicked() {
+  moveClicked()
+}
