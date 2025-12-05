4cf38f10a6ccefecfa83d8f4a144d86357b65cc3
Rainer Stropek <rainer@timecockpit.com>
2025-12-05 08:47:00 +0100

    Saved in TypeScript Playground at 2025-12-05T07:46:59.776Z

---
+++ bouncing_diamond_exam/index.ts
@@ -0,0 +1,36 @@
+let x= 150
+let y= 50
+let size= 50
+let dx= 2
+let dy= 2
+let countdown= 10
+
+function setup() {
+    createCanvas(300, 300);
+}
+
+function draw() {
+    background("gold");
+
+
+    line(x, y-size,x+size,y)
+    line(x+size, y,x,y+size)
+    line(x,y+size,x-size,y)
+    line(x-size,y,x,y-size)
+    stroke("white")
+
+    fill("black")
+    textAlign(CENTER, CENTER)
+    textSize(20)
+    text(countdown,x,y)
+
+
+    if (x <= 0 || dx >= width) {
+      }
+    if (y <= 0 || dy >= height) {
+
+    }
+
+
+
+}
