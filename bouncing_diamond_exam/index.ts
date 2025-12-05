c370ae97c7b8a9f8257eb805443ee05dd75330a6
Rainer Stropek <rainer@timecockpit.com>
2025-12-05 08:52:58 +0100

    Saved in TypeScript Playground at 2025-12-05T07:52:58.157Z

--- bouncing_diamond_exam/index.ts
+++ bouncing_diamond_exam/index.ts
@@ -1,9 +1,9 @@
-let x= 150
-let y= 50
-let size= 50
-let dx= 2
-let dy= 2
-let countdown= 10
+let x = 150
+let y = 50
+let size = 50
+let dx = 2
+let dy = 2
+let countdown = 10
 
 function setup() {
     createCanvas(300, 300);
@@ -13,23 +13,32 @@ function draw() {
     background("gold");
 
 
-    line(x, y-size,x+size,y)
-    line(x+size, y,x,y+size)
-    line(x,y+size,x-size,y)
-    line(x-size,y,x,y-size)
+    line(x, y - size, x + size, y)
+    line(x + size, y, x, y + size)
+    line(x, y + size, x - size, y)
+    line(x - size, y, x, y - size)
     stroke("white")
 
     fill("black")
     textAlign(CENTER, CENTER)
     textSize(20)
-    text(countdown,x,y)
+    text(countdown, x, y)
 
 
-    if (x <= 0 || dx >= width) {
-      }
-    if (y <= 0 || dy >= height) {
-
+    if (countdown > 0) {
+        x += dx
+        y += dy
     }
+
+
+    let hit = false
+
+    if (x + size >= 0 || x - size < 0) {
+        dx *= -1
+        
+    }
+
+
 
 
 
