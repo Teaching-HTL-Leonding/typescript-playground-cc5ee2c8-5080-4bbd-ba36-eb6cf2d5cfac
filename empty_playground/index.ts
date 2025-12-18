177dcf2353b64586a079a8221fb2425149a7d4a5
Rainer Stropek <rainer@timecockpit.com>
2025-12-18 13:33:03 +0100

    Saved in TypeScript Playground at 2025-12-18T12:33:03.458Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,4 +1,21 @@
-    let a: number =5,2
-    let b: number =2,7
+function setup() {
+    createCanvas(500, 500);
+    background("blue");
+    textAlign(CENTER, CENTER)
 
-    console. log("a= ")
+     for (let y = 0; y < 5; y++) {
+        for (let x = 0; x < 5; x++) {
+            fill("darkred");
+            noStroke();
+            circle(50, 50, 100);
+
+            fill("white");
+            text(`x=${x}, y=${y}`, 50, 50)
+            translate(100, 0)
+        }
+
+        translate(-500, 100)
+
+    }
+
+}
