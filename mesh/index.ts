2a43c9771e29aed5cffdc7132333e51ad91be587
Rainer Stropek <rainer@timecockpit.com>
2025-12-19 09:33:28 +0100

    Saved in TypeScript Playground at 2025-12-19T08:33:28.295Z

---
+++ mesh/index.ts
@@ -0,0 +1,32 @@
+const SIZE = 400; 
+const MARGIN = 50;
+                   
+
+const COLOR_HUE_OFFSET = 60
+
+function setup() {
+  createCanvas(SIZE, SIZE)
+  background("black")
+
+  strokeWeight(1)
+  colorMode(HSB)
+
+  let left = MARGIN
+  let color = 0
+  while (left <= SIZE - MARGIN) {
+    stroke(color, 100, 100)
+    let right = MARGIN
+    while (right <= SIZE - MARGIN) {
+      line(MARGIN, left, width - MARGIN, right)
+      right += MARGIN
+    }
+
+    left += MARGIN
+    color = color + COLOR_HUE_OFFSET
+  }
+}
+
+
+
+
+
