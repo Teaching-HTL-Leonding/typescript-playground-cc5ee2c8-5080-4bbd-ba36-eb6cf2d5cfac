bb9e57403efdd748b9086080202b9b5d57875d9d
Rainer Stropek <rainer@timecockpit.com>
2025-12-19 10:20:36 +0100

    Saved in TypeScript Playground at 2025-12-19T09:20:36.233Z

---
+++ chessboard/index.ts
@@ -0,0 +1,15 @@
+function setup() {
+    const SIZE = 500; // Canvas size
+    const SQUARE_SIZE = 50; // Size of a chess board square
+
+    createCanvas(SIZE, SIZE);
+    background("black");
+
+    noStroke();
+
+    let i= 0
+    while(i < 8) {
+        
+    }
+
+}
