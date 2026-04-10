8dbb3de162628a4b5dfdad294ffede881bfeccd7
Rainer Stropek <rainer@timecockpit.com>
2026-04-10 08:50:22 +0200

    Saved in TypeScript Playground at 2026-04-10T06:50:21.886Z

--- honeycombs/index.ts
+++ honeycombs/index.ts
@@ -20,13 +20,13 @@ function setup() {
     //          (20,50) ──────── (45,50)
     //
     function drawHexagon(xCoord: number, yCoord: number)
-    start// Each line() call: line(x1, y1, x2, y2)
+     // Each line() call: line(x1, y1, x2, y2)
     line(20, 0, 45, 0)
     line(45, 0, 65, 25)
     line(65, 25, 45, 50)
     line(45, 50, 20, 50)
     line(20, 50, 0, 25)
     line(0, 25, 20, 0)
+    drawHexagon(xCoord, yCoord)
 }
-
 // <<< ADD YOUR CODE HERE >>>
