c0da9722bd814be5413fa4ab8133100c94c31c2e
Rainer Stropek <rainer@timecockpit.com>
2026-04-10 09:53:30 +0200

    Saved in TypeScript Playground at 2026-04-10T07:53:29.854Z

--- honeycombs/index.ts
+++ honeycombs/index.ts
@@ -20,12 +20,13 @@ function setup() {
     //          (20,50) ──────── (45,50)
     //
   for (let x = 0; x < 5; x++) {
-        drawHexagon(x, y)
+        drawHexagon(x, 0)
         translate(90, 0)
     }
  for (let y = 0; y < 4; y++) {
-        drawHexagon(y, x)
-        translate(90, 0)
+        drawHexagon(x / 2, y)
+        translate(90, y)
+        
     }
 }
 
